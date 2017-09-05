7b4b33f68e5107f124e0aa21f59c47f151e9deea
Dustin van Hal <99022270@mydavinci.nl>
2017-09-05 16:13:11 +0200

    Helped thomas by adding stylesheet to header.php

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -17,7 +17,8 @@ class home extends CI_Controller{
     }
 
     public function index(){
-
+        $this->load->view('templates/header');
         $this->load->view('home');
+        $this->load->view('templates/footer');
     }
 }
