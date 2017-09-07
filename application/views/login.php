d854c69577b893737c7124a9f0b64dcd3ea5059a
Dustin van Hal <99022270@mydavinci.nl>
2017-09-07 22:30:15 +0200

    restyled login page

--- application/views/login.php
+++ application/views/login.php
@@ -1,12 +1,26 @@
+<!DOCTYPE html>
 <html>
-    <head>
-        <title>Studentplaza - Aanmelden</title>
+<head>
+    <title><?php echo (isset($pageTitle)) ? $pageTitle : 'StudentPlaza'; ?> </title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="/studentplaza/assets/css/style2.css"/>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body class="grijs "id="page-top">
 
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <link rel="stylesheet" type="text/css" href="/studentplaza/assets/css/style2.css"/>
-    </head>
-    <body>
-        <div class="log_bg"></div>
+< <nav class="navbar navbar-expand-lg navbar-dark bg-light fixed-top" id="mainNav">
+    <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="" onclick="window.history.back();"><span class="logo-col"><strong>Student</strong>Plaza</span></a>
+    </div>
+</nav>
+
+<div class="header">
+
+    <div class="info">
         <form class="log_container" action="" method="POST">
             <h1><strong>Student</strong>plaza</h1><br>
 
@@ -16,16 +30,33 @@
             if(isset($_SESSION['ERROR'])) {
 
                 ?>
-                <p class="label label-warning"><?php echo $_SESSION['ERROR']; ?></p>
+                <p><?php echo $_SESSION['ERROR']; ?></p>
                 <?php
             }
             ?><br/>
 
-                <input class="form-control log_txt" name="username" type="text" placeholder="Gebruikersnaam"/><br />
-                <input class="form-control log_txt" name="password" type="password" placeholder="wachtwoord"/><br />
+            <input class="form-control log_txt" name="username" type="text" placeholder="Gebruikersnaam"/><br />
+            <input class="form-control log_txt" name="password" type="password" placeholder="wachtwoord"/><br />
 
-                <input class="btn btn-default log_btn" type="submit" name="login" value="aanmelden" />
+            <input class="btn btn-default log_btn" type="submit" name="login" value="aanmelden" />
 
-            </form>
+        </form>
+
+    </div>
+</div>
+
     </body>
+<footer>
+
+
+    <div class="footer">
+        <p>&copy; Copyright 2017 by Dustin van Hal & Romy Bijkerk.</p>
+        <div class="container">
+            <a target="_blank" href="https://www.davinci.nl"><i class="fa fa-university fa-1x fa-fw"></i></a>
+            <a target="_blank" href='https://github.com/dustdustinn/StudentPlatform'><i class="fa fa-github fa-1x fa-fw"></i></a>
+
+        </div>
+    </div>
+
+</footer>
 </html>
