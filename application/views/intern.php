bbe7735a2c63759a3b13ed59b2e05ffaf2f42b0d
Dustin van Hal <99022270@mydavinci.nl>
2017-09-14 20:42:54 +0200

    trying to  figure out the layout of the intern page

--- application/views/intern.php
+++ application/views/intern.php
@@ -7,12 +7,19 @@
         </div>
     </div>
 </div>
+<div class="homecontainer">
+    <div class="headtext"></div>
+    <div class="newsfeed pull-left">
+        <h1>Home page</h1>
+        <p>op deze pagina komt een news feed achtig iets</p>
+    </div>
+    <div class="recente-thread pull-right">
+        <h3>Recente thread:</h3>
+        <p> [username]:<br> title: Dit is een thread....... [14-sep-17]</p>
 
-<div class="container">
-    <div class="headtext"><h1>Home page</h1></div>
-    <p>op deze pagina komt een news feed achtig iets</p>
+    </div>
+    <div class="jarig pull-right">
+        <h3>Gefeliciteerd:</h3>
+        <p> Bertus (23)</p>
+    </div>
 </div>
-<body>
-
-</body>
-</html>
